b642dce095ad7b7f14c9ffc2b24954baa7a7e609
K1llMan <k1llmanws@yandex.ru>
2018-08-22 17:56:03 +0300

    Добавлена возможность доступа для тестирования из локальной сети.

--- ETLService/Program.cs
+++ ETLService/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using ETLCommon;
 using ETLService.Manager;
 
+using Microsoft.Extensions.Configuration;
+
 namespace ETLService
 {
     public class Program
@@ -22,15 +24,25 @@ namespace ETLService
             BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHost BuildWebHost(string[] args) {
+            var configuration = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            var hostUrl = configuration["urls"];
+            if (string.IsNullOrEmpty(hostUrl))
+                hostUrl = "http://localhost:50828";
+
+            return WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .UseKestrel(options => {
                     options.Limits.MaxConcurrentConnections = 100;
                     options.Limits.MaxConcurrentUpgradedConnections = 100;
                 })
+                .UseUrls(hostUrl)
                 // Установка папки с сайтом
                 //.UseWebRoot("static")
                 .Build();
+        }
     }
 }
