d6e08b19845809d9283c33fcf9ec10d7436ce571
K1llMan <k1llmanws@yandex.ru>
2018-10-04 17:09:43 +0300

    Добавлен метод получения справки по api.

--- ETLService/Controllers/RootController.cs
+++ ETLService/Controllers/RootController.cs
@@ -1,10 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
-using ETLService.Manager;
-
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
 
 using Newtonsoft.Json.Linq;
+
+using ETLCommon;
+
+using ETLService.Manager;
 
 namespace ETLService.Controllers
 {
@@ -12,6 +19,55 @@ namespace ETLService.Controllers
     [Route("api")]
     public class RootController : Controller
     {
+        // GET api/methods
+        [HttpGet("methods")]
+        public object GetMethods()
+        {
+            JObject apiList = new JObject();
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            foreach (Type type in assembly.GetTypes().Where(t => t.Name.IsMatch("Controller")))
+            {
+                JArray controllerMethods = new JArray();
+
+                RouteAttribute route = (RouteAttribute)type.GetCustomAttributes(typeof(RouteAttribute)).FirstOrDefault();
+                string controllerName = type.Name.Replace("Controller", string.Empty);
+                string mainRoute = route.Template.Replace("[controller]", controllerName.ToLower());
+
+                foreach (MethodInfo method in type.GetMethods())
+                    foreach (HttpMethodAttribute attr in method.GetCustomAttributes(typeof(HttpMethodAttribute)))
+                    {
+                        JObject apiMethod = new JObject {
+                            { "route", $"{string.Join(", ", attr.HttpMethods)} {string.Join("/", new string[] { mainRoute, attr.Template }.Where(s => !string.IsNullOrEmpty(s)))}" }
+                        };
+
+                        AuthorizeAttribute authAttr = (AuthorizeAttribute)method.GetCustomAttributes(typeof(AuthorizeAttribute)).FirstOrDefault();
+                        if (authAttr != null)
+                            apiMethod.Add("auth", new JObject{
+                                { "roles", authAttr.Roles },
+                                { "policy", authAttr.Policy }
+                            });
+
+                        ParameterInfo[] parameters = method.GetParameters();
+                        if (parameters.Length > 0)
+                        {
+                            JArray methodParams = new JArray();
+                            foreach (ParameterInfo parameter in method.GetParameters())
+                                methodParams.Add(new JObject {
+                                    { "name", parameter.Name },
+                                    { "type", parameter.ParameterType.Name },
+                                    { "default", parameter.RawDefaultValue.ToString() }
+                                });
+                            apiMethod.Add("params", methodParams);
+                        }
+                        controllerMethods.Add(apiMethod);
+                    }
+
+                apiList.Add(controllerName, controllerMethods);
+            }
+
+            return apiList;
+        }
+
         // GET api/modules
         /// <summary>
         /// Возвращает доступные для отображения модули
